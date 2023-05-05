ba63505d32525d25c5ceaeb040bc3804629370ba
quispewilmer <wilmerquispecode@gmail.com>
2023-05-04 22:20:03 -0700

    Add base project

---
+++ exploraya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exploraya
+//
+//  Created by wilmer quispe on 4/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
